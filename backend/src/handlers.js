78d6d8886d74c23155910b821776ecb764877d41
papaeng89 <47139293+papaeng89@users.noreply.github.com>
2025-08-05 21:27:04 +0200

    Update handlers.js

--- backend/src/handlers.js
+++ backend/src/handlers.js
@@ -111,7 +111,7 @@ module.exports.onWebsocketConnect = (io, socket) => {
             roomObj.questionStart = Date.now();
         }
         const { imageUrl,correct, ...playerQuestion } = question;
-        socket.to(roomCode).emit(commands.ANSWERS_OPEN, { ...playerQuestion });
+        socket.to(roomCode).emit(commands.ANSWERS_OPEN, { ...playerQuestion, imageUrl});
     });
 
     //------------------------------------------------------------------------------------------------------------------
